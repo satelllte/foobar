16c1891a58d159a5b7b39507d988e5654f92b639
George <lunaerxs@gmail.com>
2022-08-02 16:51:19 +0000

    Tree part explanation

--- level-2/ion-flux-relabeling/solution.py
+++ level-2/ion-flux-relabeling/solution.py
@@ -1,3 +1,32 @@
+# Tree part:
+#
+#   P
+#  / \
+# L   R
+# 
+# If H is the tree height on the level of P, then:
+#
+# P = 2^H - 1
+# L = P - 2^(H-1)
+# R = P - 1
+# 
+# When searching a number above node X on the level H:
+#
+# if X >= P             ---> the answer is -1 (no levels above)
+# if X = L or X = R     ---> the answer is P
+# if X < L              ---> go deep to the left
+# if X > L              ---> go deep to the right
+# 
+# Example:
+#
+# | --------------------------- |
+# | Level (H) |     Nodes       |
+# | --------- | --------------- |
+# |     3     |        7        |
+# |     2     |    3       6    |
+# |     1     |  1   2   4   5  |
+# | --------------------------- |
+
 def solve(h, p, x):
     if x >= p:
         return -1
