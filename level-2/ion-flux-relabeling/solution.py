a6df4715a2adcdcb450297f096d0aa436a337103
George <lunaerxs@gmail.com>
2022-08-02 16:24:16 +0000

    level 2.1: solution

--- level-2/ion-flux-relabeling/solution.py
+++ level-2/ion-flux-relabeling/solution.py
@@ -1,3 +1,19 @@
+def solve(h, p, x):
+    if x >= p:
+        return -1
+
+    l = p - 2 ** (h - 1)
+    r = p - 1
+
+    if x == l or x == r:
+        return p
+
+    if x < l:
+        return solve(h - 1, l, x)
+
+    if x > l:
+        return solve(h - 1, r, x)
+
 def solution(h, q):
-    # your code here
-    return 0
+    p = 2 ** h - 1
+    return map(lambda x: solve(h, p, x), q)
