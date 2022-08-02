9344a1aa967d5b46d831a91d675ac2dcd6ec8b37
George <lunaerxs@gmail.com>
2022-08-02 19:17:23 +0000

    solution: finish

--- level-2/elevator-maintenance/solution.py
+++ level-2/elevator-maintenance/solution.py
@@ -1,24 +1,40 @@
 import re
 
+regexX = re.compile(r'^(\d+)$')
+regexXY = re.compile(r'^(\d+)\.(\d+)$')
+regexXYZ = re.compile(r'^(\d+)\.(\d+)\.(\d+)$')
+
+def parse(version):
+    x = -1
+    y = -1
+    z = -1
+
+    matchX = regexX.search(version)
+    matchXY = regexXY.search(version)
+    matchXYZ = regexXYZ.search(version)
+
+    if matchX:
+        x = int(matchX.group())
+    elif matchXY:
+        x, y = map(int, matchXY.groups())
+    elif matchXYZ:
+        x, y, z = map(int, matchXYZ.groups())
+    
+    return (x, y, z)
+
+def compare(a, b):
+    x1, y1, z1 = parse(a)
+    x2, y2, z2 = parse(b)
+
+    if x1 > x2: return 1
+    elif x1 < x2: return -1
+    else:
+        if y1 > y2: return 1
+        elif y1 < y2: return -1
+        else:
+            if z1 > z2: return 1
+            elif z1 < z2: return -1
+            else: return 0
+
 def solution(l):
-
-    regexXYZ = re.compile(r'^(\d+)\.(\d+)\.(\d+)$')
-    regexXY = re.compile(r'^(\d+)\.(\d+)$')
-    regexX = re.compile(r'^(\d+)$')
-
-    for version in l:
-        print('version: {}'.format(version))
-        matchXYZ = regexXYZ.search(version)
-        matchXY = regexXY.search(version)
-        matchX = regexX.search(version)
-        if matchXYZ:
-            x, y, z = map(int, matchXYZ.groups())
-            print('matchXYZ: {} | {} | {}'.format(x, y, z))
-        elif matchXY:
-            x, y = map(int, matchXY.groups())
-            print('matchXY: {} | {}'.format(x, y))
-        elif matchX:
-            x = int(matchX.group())
-            print('matchX: {}'.format(x))
-
-    return 1
+    return sorted(l, cmp=lambda a, b: compare(a, b))
