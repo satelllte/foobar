c8fe9c67cf1680c04de5df394f621464a51827bb
George <lunaerxs@gmail.com>
2022-08-02 17:48:01 +0000

    level 2.2: WIP

--- level-2/elevator-maintenance/solution.py
+++ level-2/elevator-maintenance/solution.py
@@ -1,3 +1,24 @@
+import re
+
 def solution(l):
-    # Your code here
+
+    regexXYZ = re.compile(r'^(\d+)\.(\d+)\.(\d+)$')
+    regexXY = re.compile(r'^(\d+)\.(\d+)$')
+    regexX = re.compile(r'^(\d+)$')
+
+    for version in l:
+        print('version: {}'.format(version))
+        matchXYZ = regexXYZ.search(version)
+        matchXY = regexXY.search(version)
+        matchX = regexX.search(version)
+        if matchXYZ:
+            x, y, z = map(int, matchXYZ.groups())
+            print('matchXYZ: {} | {} | {}'.format(x, y, z))
+        elif matchXY:
+            x, y = map(int, matchXY.groups())
+            print('matchXY: {} | {}'.format(x, y))
+        elif matchX:
+            x = int(matchX.group())
+            print('matchX: {}'.format(x))
+
     return 1
