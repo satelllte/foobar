8f50518e3932311443cd1b7384ac7c3ead753520
George <lunaerxs@gmail.com>
2022-08-03 17:37:00 +0000

    solution

--- level-3/fuel-injection-perfection/solution.py
+++ level-3/fuel-injection-perfection/solution.py
@@ -1,2 +1,27 @@
 def solution(n):
-    return int(n) + 1
+    n = int(n)
+
+    count = 0
+    cache = {n}
+    items = {n}
+
+    while not 1 in items:
+        next_items = set()
+
+        for x in items:
+            if x % 2 == 0:
+                if not x / 2 in cache:
+                    next_items.add(x / 2)
+                    cache.add(x / 2)
+            else:
+                if not x + 1 in cache:
+                    next_items.add(x + 1)
+                    cache.add(x + 1)
+                if not x - 1 in cache:
+                    cache.add(x - 1)
+                    next_items.add(x - 1)
+
+        items = next_items
+        count = count + 1
+
+    return count
