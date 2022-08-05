fe0fa1c9d7df599380087511203e1b21644686a1
George <lunaerxs@gmail.com>
2022-08-05 13:31:44 +0000

    foreign solution

--- level-3/doomstay-fuel/main.py
+++ level-3/doomstay-fuel/main.py
@@ -1,12 +1,15 @@
 from solution import solution
+import foreign
 
 items = [
+    [[0]],
     [[0, 2, 1, 0, 0], [0, 0, 0, 3, 4], [0, 0, 0, 0, 0], [0, 0, 0, 0,0], [0, 0, 0, 0, 0]],
     [[0, 1, 0, 0, 0, 1], [4, 0, 0, 3, 2, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]],
 ]
 
 for m in items:
     print('m: {} \nresult: {}\n-----------'.format(m, solution(m)))
+    print('m: {} \nresult: {}\n-----------'.format(m, foreign.solution(m)))
 
 assert solution(
     [
