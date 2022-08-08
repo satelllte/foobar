33c9f8ba671540cb77620d36cab67af4cfe46618
George <lunaerxs@gmail.com>
2022-08-08 15:51:49 +0000

    more test cases

--- level-3/prepare-the-bunnies-escape/main.py
+++ level-3/prepare-the-bunnies-escape/main.py
@@ -15,3 +15,23 @@ assert solution([
     [0, 1, 1, 1, 1, 1],
     [0, 0, 0, 0, 0, 0],
 ]) == 11
+
+assert solution([
+    [0, 0],
+    [0, 0],
+]) == 3
+
+assert solution([
+    [0, 1],
+    [0, 0],
+]) == 3
+
+assert solution([
+    [0, 0],
+    [1, 0],
+]) == 3
+
+assert solution([
+    [0, 1],
+    [1, 0],
+]) == 3
