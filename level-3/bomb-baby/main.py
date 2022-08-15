8ec6b2004c6f91a6f6038530534d5b9f812bd592
George <lunaerxs@gmail.com>
2022-08-15 15:57:54 +0000

    3.3: test cases

--- level-3/bomb-baby/main.py
+++ level-3/bomb-baby/main.py
@@ -1,4 +1,5 @@
 from solution import solution
 
-assert solution('4', '7') == 4
-assert solution('2', '1') == 1
+assert solution('4', '7') == '4'
+assert solution('2', '1') == '1'
+assert solution('2', '4') == 'impossible'
