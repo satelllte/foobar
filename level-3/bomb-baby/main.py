acdde7b14541404fc6af76eaff4e8aa84feb0a78
George <lunaerxs@gmail.com>
2022-08-15 16:43:50 +0000

    level 3.3: possible solution

--- level-3/bomb-baby/main.py
+++ level-3/bomb-baby/main.py
@@ -3,3 +3,5 @@ from solution import solution
 assert solution('4', '7') == '4'
 assert solution('2', '1') == '1'
 assert solution('2', '4') == 'impossible'
+assert solution('5', '3') == '3'
+assert solution('1', '4') == '3'
--- level-3/bomb-baby/solution.py
+++ level-3/bomb-baby/solution.py
@@ -1,2 +1,15 @@
 def solution(m, f):
-    return 0
+    m = int(m)
+    f = int(f)
+    step = 0
+
+    while m > 0 and f > 0:
+        step += 1
+
+        if m < f: m, f = f, m
+        m -= f
+
+        if m == 1 and f == 1:
+            return '{}'.format(step)
+
+    return 'impossible'
