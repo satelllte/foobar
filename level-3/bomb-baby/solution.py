9955706b5704b6cea67be0a618f3d897796f415e
George <lunaerxs@gmail.com>
2022-08-15 16:49:03 +0000

    fix 1

--- level-3/bomb-baby/solution.py
+++ level-3/bomb-baby/solution.py
@@ -4,12 +4,12 @@ def solution(m, f):
     step = 0
 
     while m > 0 and f > 0:
-        step += 1
+        if m == 1 and f == 1:
+            return '{}'.format(step)
 
         if m < f: m, f = f, m
         m -= f
 
-        if m == 1 and f == 1:
-            return '{}'.format(step)
+        step += 1
 
     return 'impossible'
