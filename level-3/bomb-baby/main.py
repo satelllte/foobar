b0c5437cf2272437fe957ff9895f266c12ddc95a
George <lunaerxs@gmail.com>
2022-08-15 17:23:46 +0000

    tests

--- level-3/bomb-baby/main.py
+++ level-3/bomb-baby/main.py
@@ -2,6 +2,9 @@ from solution import solution
 
 assert solution('4', '7') == '4'
 assert solution('2', '1') == '1'
+assert solution('2', '2') == 'impossible'
 assert solution('2', '4') == 'impossible'
 assert solution('5', '3') == '3'
 assert solution('1', '4') == '3'
+assert solution('1122737776', '2838294894288') == 'impossible'
+
