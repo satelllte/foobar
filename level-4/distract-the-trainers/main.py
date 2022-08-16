21ef792f18c98a33658a3c7abf8ffd149d07f668
George <lunaerxs@gmail.com>
2022-08-16 20:10:51 +0000

    is_infinite_pair

--- level-4/distract-the-trainers/main.py
+++ level-4/distract-the-trainers/main.py
@@ -1,4 +1,26 @@
-from solution import solution
+from solution import solution, is_infinite_pair, is_infinite_pair_naive, is_power_of_two, gcd
+
+# for i in range(1, 20):
+#     for j in range(1, 20):
+#         if not i == j and is_infinite_pair(i, j):
+#             print('({}, {}) --> {} | sum: {} | sum bin: {}'.format(i, j, is_infinite_pair(i, j), i + j, bin(i + j)))
+for i in range(1000, 1100):
+    for j in range(1000, 1200):
+        result = is_infinite_pair(i, j)
+        result_naive = is_infinite_pair_naive(i, j)
+        if not result == result_naive:
+            print('ERROR! ({}, {}) ===> result: {} | result_naive: {}'.format(i, j, result, result_naive))
+
+        # if not i == j and is_infinite_pair(i, j):
+        # if is_infinite_pair(i, j):
+        # print('({}, {}) --> {} | sum: {} | gcd: {} | sum/gcd: {}'.format(i, j, is_infinite_pair(i, j), i + j, gcd(i, j), (i + j) / gcd(i, j)))
+
+# print('({}, {}) --> {}'.format(9, 3, is_infinite_pair(9, 3)))
+
+# is_infinite_pair(1000, 8)
+# is_power_of_two(1)
+# for i in range(1, 65):
+#     print('is_power_of_two | {} | {}'.format(i, is_power_of_two(i)))
 
 assert solution([1, 7, 3, 21, 13, 19]) == 0
 assert solution([7, 1, 3, 21, 13, 19]) == 0
--- level-4/distract-the-trainers/solution.py
+++ level-4/distract-the-trainers/solution.py
@@ -1,2 +1,38 @@
+def gcd(x, y):
+    if y == 0:
+        return x
+    else:
+        return gcd(y, x % y)
+
+def is_power_of_two(x):
+    return x & (x - 1) == 0
+
+def is_infinite_pair_naive(x, y):
+    if x > y:
+        x, y = y, x
+
+    if x == y:
+        return False
+
+    pairs = {(x, y)}
+
+    while True:
+        y -= x
+        x *= 2
+
+        if x > y:
+            x, y = y, x
+        
+        if x == y:
+            return False
+        
+        if (x, y) in pairs:
+            return True
+        
+        pairs.add((x, y))
+
+def is_infinite_pair(x, y):
+    return not is_power_of_two((x + y) / gcd(x, y))
+
 def solution(banana_list):
     return 13183
