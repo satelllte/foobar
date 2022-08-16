e57cdee26d58c28373574de4de86d4bb5e74f67f
George <lunaerxs@gmail.com>
2022-08-16 20:33:11 +0000

    records combinations

--- level-4/distract-the-trainers/main.py
+++ level-4/distract-the-trainers/main.py
@@ -46,3 +46,95 @@ assert solution([4, 1]) == 0
 
 assert solution([3, 5]) == 2
 assert solution([5, 3]) == 2
+
+"""
+Combinations (N) = N!
+Pairs at a time (N) = floor(N/2)
+
+-----
+
+N = 1
+Elements = [1]
+
+= 1 combination
+= 0 pairs
+
+-----
+
+N = 2
+Elements = [1,2]
+
+= 2 combinations
+= 1 pair
+
+-----
+
+N = 3
+Elements = [1,2,3]
+
+[1,2]
+[1,3]
+[2,3]
+
+= 6 combinations
+= 3 pair combinations
+= 1 pair at a time
+
+-----
+
+N = 4
+Elements = [1,2,3,4]
+
+[1,2] -> [3,4]
+[1,3] -> [2,4]
+[1,4] -> [2,3]
+
+= 24 combinations
+= 3 pair combinations
+= 2 pairs at a time
+
+-----
+
+N = 5
+Elements = [1,2,3,4,5]
+
+[1,2] -> [3,4], [3,5]
+[1,3] -> [2,4], [2,5]
+[1,4] -> [2,3], [2,5]
+[1,5] -> [2,3], [2,4]
+
+= 120 combinations
+= 8 pair combinations
+= 2 pairs at a time
+
+-----
+
+N = 6
+Elements = [1,2,3,4,5,6]
+
+[1,2] -> [3,4] -> [5,6]
+[1,2] -> [3,5] -> [4,6]
+[1,2] -> [3,6] -> [4,5]
+
+[1,3] -> [2,4] -> [5,6]
+[1,3] -> [2,5] -> [4,6]
+[1,3] -> [2,6] -> [4,5]
+
+[1,4] -> [2,3] -> [5,6]
+[1,4] -> [2,5] -> [3,6]
+[1,4] -> [2,6] -> [3,5]
+
+[1,5] -> [2,3] -> [4,6]
+[1,5] -> [2,4] -> [3,6]
+[1,5] -> [2,6] -> [3,4]
+
+[1,6] -> [2,3] -> [4,5]
+[1,6] -> [2,4] -> [3,5]
+[1,6] -> [2,5] -> [3,4]
+
+= 720 combinations
+= 15 pair combinations
+= 3 pairs at a time
+
+
+"""
