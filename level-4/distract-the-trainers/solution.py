4fb633c40406b5d8168c13dff27e5bd12566a944
George <lunaerxs@gmail.com>
2022-08-22 10:03:07 +0000

    4.1: solution

--- level-4/distract-the-trainers/solution.py
+++ level-4/distract-the-trainers/solution.py
@@ -1,3 +1,5 @@
+# Technique used: https://en.wikipedia.org/wiki/Blossom_algorithm
+
 def gcd(x, y):
     while y:
         x, y = y, x % y
@@ -8,68 +10,6 @@ def is_power_of_two(x):
 
 def is_infinite_pair(x, y):
     return not is_power_of_two((x + y) / gcd(x, y))
-
-def graph_comparator(graph):
-    """
-    Compare the graph elements by the length of each of their nodes.
-    """
-    return lambda x: len(graph[x])
-
-
-def distract_the_trainers(graph):
-    """
-    Pair up the trainers in such a way that the maximum number of trainers go into an infinite thumb wrestling loop.
-    We don't pair off trainers with the same number of bananas (if we do, their game will end and they will go back to
-    work). We know enough trainer psychology to know that the one who has more bananas always gets over-confident and
-    loses. Once a match begins, the pair of trainers will continue to thumb wrestle and exchange bananas, until both
-    of them have the same number of bananas. Once that happens, both of them will lose interest and go back to
-    supervising the bunny workers, and you don't want THAT to happen!
-    The following proved useful and interesting for this implementation:
-    References:
-    Micali, S. and Vazirani, V.V., 1980, October. "An O (v| v| c| E|) algorithm for finding maximum matching in general
-    graphs". In 21st Annual Symposium on Foundations of Computer Science (SFCS 1980) (pp. 17-27). IEEE.
-    Wikipedia. Blossom Algorithm. https://en.wikipedia.org/wiki/Blossom_algorithm
-    Wolfram. Blossom Algorithm. https://mathworld.wolfram.com/BlossomAlgorithm.html
-    :param graph: a graph
-    :return: the number of trainers that have gone into an infinite thumb wrestling loop and are now distracted
-    """
-    distracted_trainers = 0
-    remaining_nodes = len(graph)
-
-    while len(graph) > 1 and remaining_nodes >= 1:
-        # Find the first min-length path in the graph. Note, there might be multiple paths of the same shortest length.
-        min_length_path = min(graph, key=graph_comparator(graph))
-
-        if (len(graph[min_length_path])) < 1:
-            del graph[min_length_path]
-        else:
-            matched_pair = [len(graph[graph[min_length_path][0]]) + 1, 1]
-
-            for node in graph[min_length_path]:
-                if len(graph[node]) < matched_pair[0]:
-                    matched_pair = [len(graph[node]), node]
-
-                for i in range(len(graph[node])):
-                    if graph[node][i] == min_length_path:
-                        # We don't pair off trainers with the same number of bananas
-                        del graph[node][i]
-                        break
-
-            for node in graph[matched_pair[1]]:
-                for i in range(len(graph[node])):
-                    if graph[node][i] == matched_pair[1]:
-                        # We don't pair off trainers with the same number of bananas
-                        del graph[node][i]
-                        break
-
-            del graph[min_length_path]
-            del graph[matched_pair[1]]
-            distracted_trainers += 2
-
-        if len(graph) > 1:
-            remaining_nodes = len(graph)
-
-    return distracted_trainers
 
 def create_graph(items):
     graph = { i: [] for i in range(len(items)) }
@@ -84,8 +24,39 @@ def create_graph(items):
 
     return graph
 
+def calculate_max_matches(graph):
+    max_matches = 0
+
+    while len(graph) > 1:
+        min_path = min(graph, key=lambda x: len(graph[x]))
+
+        if (len(graph[min_path])) < 1:
+            del graph[min_path]
+        else:
+            pair = [len(graph[graph[min_path][0]]) + 1, 1]
+
+            for node in graph[min_path]:
+                if len(graph[node]) < pair[0]:
+                    pair = [len(graph[node]), node]
+
+                for i in range(len(graph[node])):
+                    if graph[node][i] == min_path:
+                        del graph[node][i]
+                        break
+
+            for node in graph[pair[1]]:
+                for i in range(len(graph[node])):
+                    if graph[node][i] == pair[1]:
+                        del graph[node][i]
+                        break
+
+            del graph[min_path]
+            del graph[pair[1]]
+            max_matches += 2
+
+    return max_matches
+
 def solution(items):
     graph = create_graph(items)
-    distracted_trainers = distract_the_trainers(graph)
-    remaining_trainers = len(items) - distracted_trainers
-    return remaining_trainers
+    max_matches = calculate_max_matches(graph)
+    return len(items) - max_matches
