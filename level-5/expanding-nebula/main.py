30da16d6d4fe9ed2d6a104aefb6e7dadf9f9cd0f
George <lunaerxs@gmail.com>
2022-08-22 14:50:14 +0000

    rearrange tests

--- level-5/expanding-nebula/main.py
+++ level-5/expanding-nebula/main.py
@@ -1,13 +1,6 @@
 from solution import solution
 
 assert solution([
-    [True, True, False, True, False, True, False, True, True, False], 
-    [True, True, False, False, False, False, True, True, True, False], 
-    [True, True, False, False, False, False, False, False, False, True], 
-    [False, True, False, False, False, False, True, True, False, False]
-]) == 11567
-
-assert solution([
     [True, False, True],
     [False, True, False], 
     [True, False, True]
@@ -20,3 +13,10 @@ assert solution([
     [True, False, True, False, False, False, True, False], 
     [True, False, True, False, False, True, True, True]
 ]) == 254
+
+assert solution([
+    [True, True, False, True, False, True, False, True, True, False], 
+    [True, True, False, False, False, False, True, True, True, False], 
+    [True, True, False, False, False, False, False, False, False, True], 
+    [False, True, False, False, False, False, True, True, False, False]
+]) == 11567
