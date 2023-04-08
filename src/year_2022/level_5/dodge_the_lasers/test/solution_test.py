ad8663baa0e35d1db2aca0602558073ec30a474e
satelllte <lunaerxs@gmail.com>
2023-04-08 16:36:21 +0000

    solution

--- src/year_2022/level_5/dodge_the_lasers/test/solution_test.py
+++ src/year_2022/level_5/dodge_the_lasers/test/solution_test.py
@@ -1,5 +1,5 @@
 from year_2022.level_5.dodge_the_lasers.solution import solution
 
 def test():
-    assert solution('5') == 19
-    assert solution('77') == 4208
+    assert solution('5') == '19'
+    assert solution('77') == '4208'
