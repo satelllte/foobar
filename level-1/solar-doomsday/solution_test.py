7dc271db7c375cf3e2444f4336924f7fe6111347
satelllte <lunaerxs@gmail.com>
2023-04-08 09:47:01 +0000

    first test

---
+++ level-1/solar-doomsday/solution_test.py
@@ -0,0 +1,18 @@
+from solution import solution
+
+def test():
+    assert solution(1) == [1]
+    assert solution(2) == [1, 1]
+    assert solution(3) == [1, 1, 1]
+    assert solution(4) == [4]
+    assert solution(5) == [4, 1]
+    assert solution(7) == [4, 1, 1, 1]
+    assert solution(9) == [9]
+    assert solution(10) == [9, 1]
+    assert solution(12) == [9, 1, 1, 1]
+    assert solution(13) == [9, 4]
+    assert solution(1000) == [961, 36, 1, 1, 1]
+    assert solution(1001) == [961, 36, 4]
+    assert solution(15324) == [15129, 169, 25, 1]
+    assert solution(128399) == [128164, 225, 9, 1]
+    assert solution(1000000) == [1000000]
