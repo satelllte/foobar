9f54a214c87b40b8bbd0bf924ca24ffc8ecbd2b9
satelllte <lunaerxs@gmail.com>
2023-04-08 19:37:43 +0300

    Dodge the lasers (#1)

---
+++ src/year_2022/level_5/dodge_the_lasers/solution.py
@@ -0,0 +1,21 @@
+import decimal
+
+def solution(str_n):
+    n = int(str_n)
+    n = decimal.Decimal(n)
+
+    with decimal.localcontext() as decimal_ctx:
+        decimal_ctx.prec = 101
+
+        sqrt2 = decimal.Decimal(2).sqrt()
+        sqrt2plus2 = sqrt2 + decimal.Decimal(2)
+
+        # Beatty sequence
+        def solve(n):
+            if n <= 0:
+                return 0
+            bn = int(sqrt2 * n)
+            bs = int(decimal.Decimal(bn) / sqrt2plus2)
+            return ((bn * (bn + 1)) / 2) - solve(bs) - (bs * (bs + 1))
+
+        return str(int(solve(n)))
---
+++ src/year_2022/level_5/dodge_the_lasers/test/solution_test.py
@@ -0,0 +1,5 @@
+from year_2022.level_5.dodge_the_lasers.solution import solution
+
+def test():
+    assert solution('5') == '19'
+    assert solution('77') == '4208'
